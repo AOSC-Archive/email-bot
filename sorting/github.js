f6614169c232a06a996ed4b2f71933fc72729245
Lion Yang <lion@aosc.xyz>
2016-10-10 12:21:04 +0800

    github: add parser for Release messages

--- sorting/github.js
+++ sorting/github.js
@@ -9,7 +9,25 @@
   mainFilter.from(/@github\.com$/, mainFilter.forward(caseFilter))
 
   caseFilter.text(/\/releases\/tag\//, (mail) => {
-    console.log(mail.seq, mail.subject)
+    // Release Message
+    /* Example
+
+    Subject: [AOSC-Dev/bash-config] bash-config 0.4.3.0
+
+    Display hostname by default.
+
+    --
+    You are receiving this because you are subscribed to this thread.
+    View it on GitHub:
+    https://github.com/AOSC-Dev/bash-config/releases/tag/v0.4.3.0
+
+    */
+    const regexSubject = /\[(.+)\/(.+)\]/
+    const regexTag = /\/releases\/tag\/(.+)/
+    const owner = mail.subject.match(regexSubject)[1]
+    const repo = mail.subject.match(regexSubject)[2]
+    const tag = mail.text.split('-- ').pop().match(regexTag)[1]
+    console.log(`#${mail.seq}    ${repo}@${owner}: ${tag}`);
   })
 
 })()
